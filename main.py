59ef8b591384c9c3408ac1936e42f3c0c270890b
SONG <simpledeveloper.songs@gmail.com>
2021-12-21 13:50:28 +0900

    forward_to_dict 으로 전송 (not work)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # https://gist.github.com/voorloopnul/415cb75a3e4f766dc590#file-proxy-py
 
-#!/usr/bin/python
+# !/usr/bin/python
 # This is a simple port-forward / proxy, written using only the default python
 # library. If you want to make a suggestion or fix something you can contact-me
 # at voorloop_at_gmail.com
 # Distributed over IDC(I Don't Care) license
 import socket
-import select
+import select  # I/O 멀티 플렉싱을 위해 사용
 import time
 import sys
 
@@ -14,7 +14,7 @@ import sys
 # But when buffer get to high or delay go too down, you can broke things
 buffer_size = 4096
 delay = 0.0001
-forward_to = ('127.0.0.1', 8000)
+forward_to_dict = {8000: ('127.0.0.1', 8000), 10000: ('127.0.0.1', 10000)}
 
 
 class Forward:
@@ -51,22 +51,45 @@ class TheServer:
                     self.on_accept()
                     break
 
-                self.data = self.s.recv(buffer_size)
-                if len(self.data) == 0:
+                data = self.s.recv(buffer_size)
+                if len(data) == 0:
                     self.on_close()
                     break
                 else:
-                    self.on_recv()
+                    self.on_recv(data)
 
     def on_accept(self):
-        forward = Forward().start(forward_to[0], forward_to[1])
         clientsock, clientaddr = self.server.accept()
+
+        data = clientsock.recv(buffer_size)
+        request_data = data.decode().split()
+        request_method = request_data[0]
+
+        if request_method == "GET":
+            pass
+        else:
+            print("요청방법이 올바르지 않습니다")
+            return
+
+        key = int(request_data[1][1:])
+        forward_to = forward_to_dict[key]
+        print("forward_to: ", forward_to)
+        forward = Forward().start(forward_to[0], forward_to[1])
+        print(forward)
+
         if forward:
             print(clientaddr, "has connected")
             self.input_list.append(clientsock)
             self.input_list.append(forward)
             self.channel[clientsock] = forward
             self.channel[forward] = clientsock
+
+            if len(data) == 0:
+                print("close")
+            else:
+                self.on_recv(data)
+
+
         else:
             print("Can't establish connection with remote server.", end=' ')
             print("Closing connection with client side", clientaddr)
@@ -86,10 +109,9 @@ class TheServer:
         del self.channel[out]
         del self.channel[self.s]
 
-    def on_recv(self):
-        data = self.data
+    def on_recv(self, data):
         # here we can parse and/or modify the data before send forward
-        print(data)
+        print(data.decode('utf-8').split(' '))
         self.channel[self.s].send(data)
 
 
